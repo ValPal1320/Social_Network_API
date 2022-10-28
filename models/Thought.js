0fed68829f83266dd94fc951f7d258ea018c624f
Val Pal <valgonzalez13@yahoo.com>
2022-10-27 20:31:40 -0500

    deleted Reaction.js and moved ReactionSchema into Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -2,7 +2,38 @@
 const { Schema, model } = require('mongoose');
 const moment = require('moment');
 
-const ReactionSchema = require('./Reaction')
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => {
+                return new Types.ObjectId()
+            },
+        },
+        reactionBody: {
+            type: String,
+            required: () => {
+                return this.reactionBody.length <= 280
+            }
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: [{
+            type: Date,
+            default: Date.now,
+            get: (createdAt) => {
+                return moment(createdAt).format("MMMM Do YYYY, h:mm:ss")
+            }
+        }]
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+);
 
 const ThoughtSchema = new Schema(
     {
